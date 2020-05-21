7aefc14d2abc0de7f653c1b082bd8368db0997ec
mascio01 <65715911+mascio01@users.noreply.github.com>
2020-05-21 14:24:30 +0200

    Create mainpage.xaml.cs

---
+++ mainpage.xaml.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace mascella.nicolo._5g.localimg
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : TabbedPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
